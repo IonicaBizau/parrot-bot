d8dbf1ba47fb72f5dacee4ea73c1bf4fb0d71b5b
Ionică Bizău <bizauionica@gmail.com>
2016-07-14 15:58:12 +0300

    JSDoc comments

--- example/interactive.js
+++ example/interactive.js
@@ -7,22 +7,36 @@ const Parrot = require("..")
 // Create a new bot
 let bot = new Parrot("en");
 
+prompt.colors = false;
+prompt.delimiter = "";
+prompt.message = ""
 prompt.start();
 
 let commands = {
-    die: () => bot.die()
+    die: () => {
+        bot.die();
+        process.exit();
+    }
   , clear_cache: () => bot.clearCache()
+  , "new": (name, lang) => {
+        try {
+            bot = new Parrot(lang, { name: name });
+            console.log(`Now you're talking to ${name}.`);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 };
 
 let doAsync = () => {
-    prompt.get("message", (err, result) => {
+    prompt.get(">", (err, result) => {
         if (err) { return console.error(err); }
 
-        result = result.message;
+        result = result[">"];
         if (result.startsWith("/")) {
-            let com = result.substring(1);
-            if (commands[com]) {
-                commands[com]();
+            let com = result.substring(1).split(" ");
+            if (commands[com[0]]) {
+                commands[com[0]].apply(this, com.slice(1));
             } else {
                 console.log(`Invalid command: ${result}. Available commands are: ${Object.keys(commands).join(", ")}`);
             }
@@ -35,6 +49,44 @@ let doAsync = () => {
             doAsync();
         });
     });
-}
+};
+
+console.log(`
+    Welcome to the ParrotBot shell.
+    ===============================
+    Here you can talk and teach a real robot. :D
+
+    Simply write your message and press <Enter>.
+    If you submit an empty message, the robot will
+    probably decide to ask you a question.
+
+    If you want to run a specific action on the
+    robot, use the commands (messages starting with "/").
+
+    Available commands:
+
+        /new <name> <language>
+          Creates a new robot. Don't forget to give
+          him/her a name. You can use this feature
+          to teach a specific robot things on a known
+          subject (e.g. physics).
+
+          If there is already a robot with this name
+          and language, the memory will taken from
+          the database.
+
+        /die
+          Kills the current robot. This will not
+          delete their memory.
+
+        /clearCache
+          Clears the current robot internal cache.
+
+    By default, you're talking to Alice.
+
+                          - - -
+
+                        E n j o y !
+`);
 
 doAsync();
